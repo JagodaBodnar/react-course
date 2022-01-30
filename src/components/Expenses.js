ff152e0de34f1fe53b4d39fdbac0191e9d87aeb7
JagodaBodnar <jagodabodnar90@gmail.com>
2022-01-31 00:12:37 +0100

    Finishing project

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,6 +4,7 @@ import Card from './UI/Card';
 import './Expenses.css';
 import ExpensesFilter from "./NewExpense/ExpenseFilter";
 import ExpensesList from "./NewExpense/ExpensesList";
+import ExpensesChart from "./NewExpense/ExpensesChart";
 
 const Expenses = ({items}) => {
     const [selectedYear, setSelectedYear] = useState('')
@@ -16,6 +17,7 @@ const Expenses = ({items}) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={selectedYear} onFilterYear={filterYearHandler}/>
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList expenses={filteredExpenses}/>
         </Card>
 
